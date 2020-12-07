6788f31492fe1aed751c40c49db972775d0b4cb7
namanegi <namanegippoi@gmail.com>
2020-12-07 20:26:57 +0900

    1

--- src/headerapp.js
+++ src/headerapp.js
@@ -2,7 +2,7 @@ function HeaderApp() {
     return (
         <div class="headerapp">
             <header id="top">
-                <div id="logo"><img src="./logo.png" alt="" /></div>
+                <div id="logo"><img id="logo-img" src="./logo.png" alt="" /></div>
                 <div id="code">金融機関コード：0917</div>
                 <div id="mojisize">文字サイズ：</div>
                 <div class="ch-moji-size"><img src="./fontsize_s.png" alt="" /></div>
