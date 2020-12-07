83c56fab0c90115f8a307f29732db470c30004ad
namanegi <namanegippoi@gmail.com>
2020-12-07 17:51:54 +0900

    1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <HeaderApp />,
-  document.getElementById('headerroot');
+  document.getElementById('headerroot')
   /*
   <React.StrictMode>
     <App />
