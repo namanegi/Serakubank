c2bc88872a1b0a22df9d5f8be251cd3f611532e2
namanegi <namanegippoi@gmail.com>
2020-12-07 17:47:24 +0900

    1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import App from './App';
+import HeaderApp from './headerapp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <HeaderApp />,
+  document.getElementById('headerroot');
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root');
 );
 
 // If you want to start measuring performance in your app, pass a function
