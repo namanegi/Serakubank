c95d03bdd241b57d92f4f9f313679038c053e5c9
namanegi <namanegippoi@gmail.com>
2020-12-07 17:56:05 +0900

    1

--- src/headerapp.js
+++ src/headerapp.js
@@ -2,12 +2,12 @@ function HeaderApp() {
     return (
         <div class="headerapp">
             <header id="top">
-                <div id="logo"><img src="./logo.png"></div>
+                <div id="logo"><img src="./logo.png" /></div>
                 <div id="code">金融機関コード：0917</div>
                 <div id="mojisize">文字サイズ：</div>
-                <div class="ch-moji-size"><img src="./fontsize_s.png"></div>
-                <div class="ch-moji-size"><img src="./fontsize_m.png"></div>
-                <div class="ch-moji-size"><img src="./fontsize_l.png"></div>
+                <div class="ch-moji-size"><img src="./fontsize_s.png" /></div>
+                <div class="ch-moji-size"><img src="./fontsize_m.png" /></div>
+                <div class="ch-moji-size"><img src="./fontsize_l.png" /></div>
                 <div id="map">
                     <ul>
                         <li><a href="">global site</a></li>
@@ -15,9 +15,9 @@ function HeaderApp() {
                     </ul>
                 </div>
                 <div id="search-bar">
-                    <form action="cgi-bin/example.cgi" method="post">
-                        <input type="search" name="search" id="search-input" placeholder="検索ワードを入力">
-                        <input type="image" src="./search.png" name="submit" id="search-button">
+                    <form action="" method="post">
+                        <input type="search" name="search" id="search-input" placeholder="検索ワードを入力"></input>
+                        <input type="image" src="./search.png" name="submit" id="search-button"></input>
                     </form>
                 </div>
             </header>
