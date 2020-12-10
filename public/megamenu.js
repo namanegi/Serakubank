a0c17740f5d02010fc735267a889064140b3039d
namanegi <namanegippoi@gmail.com>
2020-12-10 14:24:53 +0900

    11

--- public/megamenu.js
+++ public/megamenu.js
@@ -1,7 +1,16 @@
 $(function() {
     $('.nav-btn').hover(function() {
         $(this).next().slideDown();
+        $(this).css('background-color', 'rgb(54, 155, 158)');
     }, function() {
-        $(this).next().stop().hide();
-    })
+        $(this).next().hide();
+        $(this).css('background-color',  'rgb(38,45,123)');
+    });
+    $('.megamenu').hover(function() {
+        $(this).show();
+        $(this).prev().css('background-color', 'rgb(54, 155, 158)');
+    }, function() {
+        $(this).hide();
+        $(this).prev().css('background-color',  'rgb(38,45,123)');
+    });
 })
